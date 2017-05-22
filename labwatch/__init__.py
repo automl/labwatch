00c3878f09162c658c4f32960879d95ea4f70874
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2017-05-22 17:05:50 +0200

    update setup py

--- labwatch/__init__.py
+++ labwatch/__init__.py
@@ -4,3 +4,4 @@ from __future__ import division, print_function, unicode_literals
 
 __version__ = "0.1dev"
 __authors__ = ["Jost Tobias Springenberg", "Aaron Klein", "Klaus Greff"]
+__url__ = "https://github.com/stokasto/labwatch"
