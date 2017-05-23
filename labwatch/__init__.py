b8ed18f5efbada0dc3c267f63af9fb36bbb47888
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2017-05-23 11:05:37 +0200

    changed path

--- labwatch/__init__.py
+++ labwatch/__init__.py
@@ -4,4 +4,4 @@ from __future__ import division, print_function, unicode_literals
 
 __version__ = "0.1dev"
 __authors__ = ["Jost Tobias Springenberg", "Aaron Klein", "Klaus Greff"]
-__url__ = "https://github.com/stokasto/labwatch"
+__url__ = "https://github.com/automl/labwatch"
